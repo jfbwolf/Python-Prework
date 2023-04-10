e656bd85fcf66e5790c0d1e161d058515d92f385
James Boyles <wolvesdenbusiness@gmail.com>
2023-04-10 17:26:48 -0400

    first commit

---
+++ prework.py
@@ -0,0 +1,7 @@
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
+
+def hello_name(username):
+    """Display a simple greeting"""
+    print("hello_" + username.upper() + "!")
+    
+hello_name('username')
